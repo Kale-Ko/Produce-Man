3fa3c27bc2d6255dab7e6cd3327da665b0ac177d
OrangeKale64 <54416665+OrangeKale64@users.noreply.github.com>
2021-03-12 12:09:09 -0500

    Update

---
+++ src/com/kale_ko/api/java/Console.java
@@ -0,0 +1,35 @@
+package com.kale_ko.api.java;
+
+import com.kale_ko.snake.Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Console {
+    public static void log(String message) {
+        System.out.println(ConsoleColors.RESET + message);
+    }
+
+    public static void warn(String message) {
+        System.out.println(ConsoleColors.RESET + ConsoleColors.YELLOW_BOLD + "[Warn] " + ConsoleColors.RESET + ConsoleColors.YELLOW + message);
+    }
+
+    public static void error(String message) {
+        System.out.println(ConsoleColors.RESET + ConsoleColors.RED_BOLD + "[Error] " + ConsoleColors.RESET + ConsoleColors.RED + message);
+    }
+
+    public static void debug(String message, int level) {
+        if (level == 2 && Main.debug.equalsIgnoreCase("full")) {
+            System.out.println(ConsoleColors.RESET + ConsoleColors.PURPLE_BOLD + "[Debug] " + ConsoleColors.RESET + ConsoleColors.PURPLE + message);
+        } else if (level == 1 && Main.debug.equalsIgnoreCase("low") || Main.debug.equalsIgnoreCase("full"))  {
+            System.out.println(ConsoleColors.RESET + ConsoleColors.PURPLE_BOLD + "[Debug] " + ConsoleColors.RESET + ConsoleColors.PURPLE + message);
+        }
+    }
+
+    public static String getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        return reader.readLine();
+    }
+}
