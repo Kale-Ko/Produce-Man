52eda3b07f3b13cfde27df34a25a32ec373b64db
Kale-Ko <github@kaleko.dev>
2022-02-16 21:08:39 -0500

    Update

--- src/main/java/com/kale_ko/api/java/ConsoleColors.java
+++ src/main/java/com/kale_ko/api/java/ConsoleColors.java
@@ -1,10 +1,3 @@
-/**
-    @license
-    MIT License
-    Copyright (c) 2021 Kale Ko
-    See https://kaleko.ga/license.txt
-*/
-
 package com.kale_ko.api.java;
 
 public class ConsoleColors {
--- src/main/java/com/kale_ko/produceman/DebugModes.java
+++ src/main/java/com/kale_ko/produceman/DebugModes.java
@@ -1,10 +1,3 @@
-/**
-    @license
-    MIT License
-    Copyright (c) 2021 Kale Ko
-    See https://kaleko.ga/license.txt
-*/
-
 package com.kale_ko.produceman;
 
 public enum DebugModes {
